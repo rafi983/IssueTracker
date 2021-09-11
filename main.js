85178d125fb22bf20bea0e864cbc26f0686b5d1b
rafi983 <rafiirfan211@gmail.com>
2021-09-11 19:59:06 +0600

    js fixed

--- main.js
+++ main.js
@@ -25,12 +25,10 @@ function submitIssue(e) {
 
 const setStatusClosed = (id) => {
   const issues = JSON.parse(localStorage.getItem("issues"));
-  const currentIssue = issues.find((issue) => parseInt(issue.id) === id);
+  const currentIssue = issues.find((issue) => issue.id == id);
   currentIssue.status = "Closed";
   localStorage.setItem("issues", JSON.stringify(issues));
   fetchIssues();
-  const description = document.getElementById("description");
-  description.innerHTML = `<del>${description.innerText}</del>`;
 };
 
 const deleteIssue = (id) => {
@@ -42,20 +40,27 @@ const deleteIssue = (id) => {
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem("issues"));
+  if (!issues) {
+    return;
+  }
   const issuesList = document.getElementById("issuesList");
   issuesList.innerHTML = "";
-
   for (var i = 0; i < issues.length; i++) {
     const { id, description, severity, assignedTo, status } = issues[i];
-
-    issuesList.innerHTML += `<div class="well">
+    issuesList.innerHTML += `<div id= ${id} class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3 id="description"> ${description} </h3>
+                              <h3 id = ${id}_title> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
-                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              <button onclick="setStatusClosed(${id})" class="btn btn-warning">Close</button>
+                              <button onclick="deleteIssue(${id})" class="btn btn-danger">Delete</button>
                               </div>`;
+    if (issues[i].status == "Closed") {
+      document.getElementById(`${id}_title`).style.textDecoration =
+        "line-through";
+      document.getElementById(`${id}_title`).style.textDecorationColor =
+        "black";
+    }
   }
 };
